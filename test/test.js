f069fac70ba03b3ecc47fe57fee8314e28b78bc6
Matthew Tole <mtole@palmerlegaltech.com>
2013-01-22 19:28:18 +0000

    Add test for custom sounds and rework test structure.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
-var winston = require('winston');
 var should = require('should')
+var winston = require('winston')
 
 require('../index').Pushover;
 
@@ -10,17 +10,40 @@ var options = {
   token: 'kBV2sDHg59tdAHJBPLFamp77Id0OWW'
 }
 
-winston.remove(winston.transports.Console)
-winston.add(winston.transports.Pushover, options)
-
 describe('Winston Pushover', function () {
+
+  var logger = null
+
+  beforeEach(function (done) {
+    logger = new (winston.Logger)()
+    done()
+  })
+
+  afterEach(function (done) {
+    setTimeout(done, 1000)
+  })
 
   describe('logging', function () {
 
     var testMessage = 'winston-pushover test'
 
     it('should send message to Pushover', function (done) {
-      winston.warn(testMessage, { metadata: 'some data' }, function (err, level, msg, meta) {
+      logger.add(winston.transports.Pushover, options)
+
+      logger.warn(testMessage, { metadata: 'some data' }, function (err, level, msg, meta) {
+        should.not.exist(err)
+        level.should.equal('warn')
+        msg.should.equal(testMessage)
+        done()
+      })
+    })
+
+    it('should accept custom sounds', function (done) {
+      options.sound = 'magic'
+      logger.add(winston.transports.Pushover, options)
+      delete options['magic']
+
+      logger.warn(testMessage, { metadata: 'some data' }, function (err, level, msg, meta) {
         should.not.exist(err)
         level.should.equal('warn')
         msg.should.equal(testMessage)
